32403a66ee607d8ec5e91130813cd9b071d2a524
afmmars <anastasf/2gmail.com>
2020-02-11 16:04:15 +0300

    update workspace

--- checkout.ps1
+++ checkout.ps1
@@ -1,3 +1,9 @@
 #!/usr/local/bin/pwsh
 
 git clone git@github.com:iquipsys-positron/iqs-scripts-management-ps
+
+git clone git@github.com:pip-services/pip-services-facade-ps.git
+git clone git@github.com:pip-services-infrastructure/pip-facade-infrastructure-ps.git
+git clone git@github.com:pip-services-users/pip-facade-users-ps.git
+git clone git@github.com:pip-services-content/pip-facade-content-ps.git
+git clone git@github.com:pip-services-support/pip-facade-support-ps.git
